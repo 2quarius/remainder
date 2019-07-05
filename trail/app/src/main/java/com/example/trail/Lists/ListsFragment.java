8d50d18a6738de8c9463fd73bd34cb1a2c689092
SHINNYCHOU <1437947307@qq.com>
2019-07-05 15:24:25 +0800

    drag list

--- trail/app/src/main/java/com/example/trail/Lists/ListsFragment.java
+++ trail/app/src/main/java/com/example/trail/Lists/ListsFragment.java
@@ -1,44 +1,61 @@
 package com.example.trail.Lists;
 
 
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.trail.R;
 
+import java.util.Collections;
+import java.util.List;
+
 
 public class ListsFragment extends Fragment {
+    private ContentAdapter adapter;
+    private String[] mPlaces;
+    private String[] mPlaceDesc;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         RecyclerView recyclerView = (RecyclerView) inflater.inflate(
                 R.layout.recycler_view, container, false);
-        ContentAdapter adapter = new ContentAdapter(recyclerView.getContext());
+        adapter = new ContentAdapter(recyclerView.getContext());
         recyclerView.setAdapter(adapter);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        helper.attachToRecyclerView(recyclerView);
         return recyclerView;
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView name;
         public TextView description;
+        public CardView card;
         public ViewHolder(LayoutInflater inflater, ViewGroup parent) {
             super(inflater.inflate(R.layout.fragment_lists, parent, false));
-            CheckBox checkBox = (CheckBox) itemView.findViewById(R.id.checkBox);
+            final CheckBox checkBox = (CheckBox) itemView.findViewById(R.id.checkBox);
+            card=(CardView) itemView.findViewById(R.id.card_view);
             name = (TextView) itemView.findViewById(R.id.card_title);
             description = (TextView) itemView.findViewById(R.id.card_text);
             itemView.setOnClickListener(new View.OnClickListener() {
@@ -47,23 +64,36 @@ public class ListsFragment extends Fragment {
                     System.out.println("click");
                 }
             });
+            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+
+                @Override
+                public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+                    if(checkBox.isChecked()){
+                        //card.setCardBackgroundColor();
+                        //如果是分开的已完成和未完成，此处删掉未完成的，增加已完成的数组
+                    }
+                }
+            });
         }
     }
 
     /**
      * Adapter to display recycler view.
      */
-    public static class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
+    public class ContentAdapter extends RecyclerView.Adapter<ViewHolder> {
         // Set numbers of Card in RecyclerView.
-        private static final int LENGTH = 18;
+        private static final int LENGTH = 8;
 
-        private final String[] mPlaces;
-        private final String[] mPlaceDesc;
+//        private final String[] mPlaces;
+//        private final String[] mPlaceDesc;
+        /*private final String[] finish;
+*/
 
         public ContentAdapter(Context context) {
             Resources resources = context.getResources();
             mPlaces = resources.getStringArray(R.array.places);
             mPlaceDesc = resources.getStringArray(R.array.place_desc);
+           // finish=resources.getStringArray(R.array.)
         }
 
         @Override
@@ -73,6 +103,7 @@ public class ListsFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(ViewHolder holder, int position) {
+            //System.out.println(position);
             holder.name.setText(mPlaces[position % mPlaces.length]);
             holder.description.setText(mPlaceDesc[position % mPlaceDesc.length]);
         }
@@ -82,4 +113,86 @@ public class ListsFragment extends Fragment {
             return LENGTH;
         }
     }
+    ItemTouchHelper helper = new ItemTouchHelper(new ItemTouchHelper.Callback() {
+        @Override
+        public int getMovementFlags(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+            int dragFrlg = 0;
+            if (recyclerView.getLayoutManager() instanceof GridLayoutManager){
+                //System.out.println("grid");
+                dragFrlg = ItemTouchHelper.UP|ItemTouchHelper.DOWN|ItemTouchHelper.LEFT|ItemTouchHelper.RIGHT;
+            }else if(recyclerView.getLayoutManager() instanceof LinearLayoutManager){
+                //System.out.println("grid");
+                dragFrlg = ItemTouchHelper.UP|ItemTouchHelper.DOWN;
+            }
+            return makeMovementFlags(dragFrlg,0);
+        }
+
+        @Override
+        public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+            //滑动事件  下面注释的代码，滑动后数据和条目错乱，被舍弃
+//            Collections.swap(datas,viewHolder.getAdapterPosition(),target.getAdapterPosition());
+//            ap.notifyItemMoved(viewHolder.getAdapterPosition(),target.getAdapterPosition());
+            //得到当拖拽的viewHolder的Position
+            int fromPosition = viewHolder.getAdapterPosition();
+            //拿到当前拖拽到的item的viewHolder
+            int toPosition = target.getAdapterPosition();
+            if (fromPosition < toPosition) {
+                for (int i = fromPosition; i < toPosition; i++) {
+                    String tmp=mPlaceDesc[i];
+                    mPlaceDesc[i]=mPlaceDesc[i+1];
+                    mPlaceDesc[i+1]=tmp;
+                    tmp=mPlaces[i];
+                    mPlaces[i]=mPlaces[i+1];
+                    mPlaces[i+1]=tmp;
+
+                }
+            } else {
+                for (int i = fromPosition; i > toPosition; i--) {
+                    String tmp=mPlaceDesc[i];
+                    mPlaceDesc[i]=mPlaceDesc[i-1];
+                    mPlaceDesc[i-1]=tmp;
+                    tmp=mPlaces[i];
+                    mPlaces[i]=mPlaces[i-1];
+                    mPlaces[i-1]=tmp;
+                }
+            }
+            adapter.notifyItemMoved(fromPosition, toPosition);
+            return true;
+        }
+
+        @Override
+        public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+            //侧滑删除可以使用；
+        }
+
+        @Override
+        public boolean isLongPressDragEnabled() {
+            return true;
+        }
+        /**
+         * 长按选中Item的时候开始调用
+         * 长按高亮
+         * @param viewHolder
+         * @param actionState
+         */
+        @Override
+        public void onSelectedChanged(RecyclerView.ViewHolder viewHolder, int actionState) {
+            if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+                viewHolder.itemView.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
+            }
+            super.onSelectedChanged(viewHolder, actionState);
+        }
+
+        /**
+         * 手指松开的时候还原高亮
+         * @param recyclerView
+         * @param viewHolder
+         */
+        @Override
+        public void clearView(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder) {
+            super.clearView(recyclerView, viewHolder);
+            viewHolder.itemView.setBackgroundColor(0);
+            adapter.notifyDataSetChanged();  //完成拖动后刷新适配器，这样拖动后删除就不会错乱
+        }
+    });
 }
