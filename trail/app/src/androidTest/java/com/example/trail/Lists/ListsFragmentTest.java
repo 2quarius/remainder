7f465e146de8be743b396e10e349ed99e6f11ad5
2quarius <863115889@qq.com>
2019-07-10 19:50:09 +0800

    calendar test

--- trail/app/src/androidTest/java/com/example/trail/Lists/ListsFragmentTest.java
+++ trail/app/src/androidTest/java/com/example/trail/Lists/ListsFragmentTest.java
@@ -1,8 +1,5 @@
 package com.example.trail.Lists;
 
-import androidx.fragment.app.ListFragment;
-import androidx.test.rule.ActivityTestRule;
-
 import com.example.trail.MainActivity;
 
 import org.junit.Before;
@@ -11,8 +8,6 @@ import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
-import static org.junit.Assert.*;
 import com.android21buttons.fragmenttestrule.FragmentTestRule;
 import com.example.trail.R;
 import com.example.trail.Utils.RecyclerViewMatcher;
