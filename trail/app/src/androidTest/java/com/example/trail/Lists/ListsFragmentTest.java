2645e78bc9b5739899a0826e829ca8cce0f173d3
2quarius <863115889@qq.com>
2019-07-10 16:29:03 +0800

    lists

---
+++ trail/app/src/androidTest/java/com/example/trail/Lists/ListsFragmentTest.java
@@ -0,0 +1,33 @@
+package com.example.trail.Lists;
+
+import androidx.fragment.app.ListFragment;
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.trail.MainActivity;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.junit.Assert.*;
+import com.android21buttons.fragmenttestrule.FragmentTestRule;
+import com.example.trail.R;
+import com.example.trail.Utils.RecyclerViewMatcher;
+
+public class ListsFragmentTest {
+    @Rule
+    public FragmentTestRule<MainActivity,ListsFragment> rule = new FragmentTestRule<>(MainActivity.class,ListsFragment.class);
+    @Before
+    public void setUp() throws Exception {
+    }
+    @Test
+    public void onCreate(){
+        onView(withRecyclerView(R.id.my_recycler_view).atPosition(3)).perform(click());
+    }
+    public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
+        return new RecyclerViewMatcher(recyclerViewId);
+    }
+}
