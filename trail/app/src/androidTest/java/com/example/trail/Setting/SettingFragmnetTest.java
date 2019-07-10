01a6da520a519d4e4eefff1ca426e2da369bdb01
Saturn0324 <Forever0324@sjtu.edu.cn>
2019-07-10 15:55:15 +0800

    save

--- trail/app/src/androidTest/java/com/example/trail/Setting/SettingFragmnetTest.java
+++ trail/app/src/androidTest/java/com/example/trail/Setting/SettingFragmnetTest.java
@@ -1,24 +1,19 @@
 package com.example.trail.Setting;
 
-import android.content.Intent;
-import android.view.View;
 
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.test.espresso.ViewInteraction;
 import androidx.test.rule.ActivityTestRule;
 
 import com.example.trail.MainActivity;
 import com.example.trail.R;
+import com.example.trail.Utility.TabConstants;
 
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
-import static androidx.test.espresso.matcher.ViewMatchers.isFocusable;
+import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
@@ -27,29 +22,25 @@ public class SettingFragmnetTest {
     @Rule
     public ActivityTestRule<MainActivity> rule=new ActivityTestRule<>(MainActivity.class);
 
-    @Before
-    public void setUp() throws Exception {
-    }
-
     @Test
     public void onCreateView() {
-//        onView(withId(R.id.card_title)).perform(click());
-//        FragmentManager fm;
-//        fm=rule.getActivity().getSupportFragmentManager();
-//        FragmentTransaction ft;
-//        ft=fm.beginTransaction();
-//        SettingFragmnet settingFragmnet=new SettingFragmnet();
-//        ft.replace(R.layout.fragment_lists,settingFragmnet);
-//        onView(withId(R.id.btn_about)).perform(click());
-        onView(withText())
-        onView(withId(R.id.btn_about)).perform(click());
+        onView(withId(R.id.fab_addTask)).check(matches(isClickable()));
     }
 
     @Test
     public void onViewCreated() {
+        onView(withText(TabConstants.SETTING.getTitle())).perform(click());
+        onView(withId(R.id.btn_about)).check(matches(isClickable()));
+        onView(withId(R.id.btn_language)).check(matches(isClickable()));
+        onView(withId(R.id.btn_voice)).check(matches(isClickable()));
+        onView(withId(R.id.btn_accountSetting)).check(matches(isClickable()));
+        onView(withId(R.id.btn_theme)).check(matches(isClickable()));
+        onView(withId(R.id.btn_information)).check(matches(isClickable()));
+        onView(withId(R.id.fab_addTask)).check(matches(isClickable()));
     }
 
     @Test
     public void onDestroyView() {
+        onView(withId(R.id.fab_addTask)).check(matches(isClickable()));
     }
 }
