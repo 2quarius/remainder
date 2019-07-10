6a8d148ed3346558b2ff33e459df3f41bbd65726
Mosen Cao <979077305@qq.com>
2019-07-10 15:45:47 +0800

    AddTask a little change

---
+++ trail/app/src/androidTest/java/com/example/trail/NewTask/AddTaskActivityTest.java
@@ -0,0 +1,30 @@
+package com.example.trail.NewTask;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddTaskActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onDateSet() {
+    }
+
+    @Test
+    public void onTimeSet() {
+    }
+}
