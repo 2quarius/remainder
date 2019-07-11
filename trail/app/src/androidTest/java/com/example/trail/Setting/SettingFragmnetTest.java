626834e1ff4668345aba8e7d27fce9eac3117a38
Saturn0324 <Forever0324@sjtu.edu.cn>
2019-07-11 11:00:09 +0800

    voice set

--- trail/app/src/androidTest/java/com/example/trail/Setting/SettingFragmnetTest.java
+++ trail/app/src/androidTest/java/com/example/trail/Setting/SettingFragmnetTest.java
@@ -11,9 +11,11 @@ import org.junit.Rule;
 import org.junit.Test;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.assertion.ViewAssertions.*;
 import static androidx.test.espresso.matcher.ViewMatchers.isClickable;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
@@ -30,13 +32,36 @@ public class SettingFragmnetTest {
     @Test
     public void onViewCreated() {
         onView(withText(TabConstants.SETTING.getTitle())).perform(click());
-        onView(withId(R.id.btn_about)).check(matches(isClickable()));
-        onView(withId(R.id.btn_language)).check(matches(isClickable()));
-        onView(withId(R.id.btn_voice)).check(matches(isClickable()));
-        onView(withId(R.id.btn_accountSetting)).check(matches(isClickable()));
-        onView(withId(R.id.btn_theme)).check(matches(isClickable()));
-        onView(withId(R.id.btn_information)).check(matches(isClickable()));
-        onView(withId(R.id.fab_addTask)).check(matches(isClickable()));
+        onView(withId(R.id.btn_about))
+                .check(matches(isDisplayed()))
+                .check(matches(isClickable()))
+                .perform(click());
+        pressBack();//back to setting
+        onView(withId(R.id.btn_language))
+                .check(matches(isDisplayed()))
+                .check(matches(isClickable()))
+                .perform(click());
+        pressBack();//back
+        onView(withId(R.id.btn_accountSetting))
+                .check(matches(isDisplayed()))
+                .check(matches(isClickable()))
+                .perform(click());
+        pressBack();//back
+        onView(withId(R.id.btn_voice))
+                .check(matches(isDisplayed()))
+                .check(matches(isClickable()))
+                .perform(click());
+        pressBack();//back
+        onView(withId(R.id.btn_theme))
+                .check(matches(isDisplayed()))
+                .check(matches(isClickable()))
+                .perform(click());
+        pressBack();//back
+        onView(withId(R.id.btn_information))
+                .check(matches(isDisplayed()))
+                .check(matches(isClickable()))
+                .perform(click());
+        pressBack();//back
     }
 
     @Test
