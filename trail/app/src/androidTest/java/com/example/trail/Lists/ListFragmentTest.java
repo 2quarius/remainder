b9dbdf70d21d6d3166c4e2675e5bce7358083bd8
SHINNYCHOU <1437947307@qq.com>
2019-07-11 09:24:15 +0800

    save0711

--- trail/app/src/androidTest/java/com/example/trail/Lists/ListFragmentTest.java
+++ trail/app/src/androidTest/java/com/example/trail/Lists/ListFragmentTest.java
@@ -20,11 +20,13 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.longClick;
 import static androidx.test.espresso.action.ViewActions.swipeLeft;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isChecked;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withParent;
@@ -51,7 +53,11 @@ public class ListFragmentTest {
     public void clickCheckBox() throws Exception {
         //onView(allOf(withParent(withId(R.layout.recycler_view))qq,withId(R.id.card_view))).perform(scrollToPosition(4));
        //onView(allOf(withParent(withId(R.id.list_recycler_view)),withId(R.id.card_view))).perform(longClick());
-        onView(withRecyclerView(R.id.my_recycler_view).atPosition(0)).perform();
+        onView(withRecyclerView(R.id.my_recycler_view).atPosition(0)).perform(longClick());
+//        onView(withRecyclerView(R.id.my_recycler_view).atPosition(0)).onChildView(withId(R.id.card_view)).onChildView(withId(R.id.checkBox))
+//                .perform(click())
+//                .check(matches(isChecked()));
+
         //        //onView(withId(R.id.checkBox)).check(matches(isDisplayed()));
        // onView(withId(R.id.my_recycler_view)).perform(RecyclerViewActions.actionOnItemAtPosition(2,));
 
@@ -59,7 +65,7 @@ public class ListFragmentTest {
     }
     @Test
     public void swipeDelete() throws Exception {
-        onView(withRecyclerView(R.id.my_recycler_view).atPosition(3))
+        onView(withRecyclerView(R.id.my_recycler_view).atPosition(1))
                 .check(matches(isDisplayed()))
                 .perform(swipeLeft());
         //onView(withRecyclerView(R.id.my_recycler_view).atPosition(3)).perform(swipeLeft());
@@ -68,7 +74,7 @@ public class ListFragmentTest {
     @Test
     public void DragItem() throws Exception {
         onView(withRecyclerView(R.id.my_recycler_view).atPosition(0)).perform(longClick());
-        RecyclerViewActions.actionOnItemAtPosition(0,longClick());
+        //RecyclerViewActions.actionOnItemAtPosition(0,longClick());
 
     }
     @Test
