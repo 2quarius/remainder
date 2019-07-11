2e295d35643978603be6ddd1d46b343cba90a584
2quarius <863115889@qq.com>
2019-07-11 11:07:14 +0800

    almost test

---
+++ trail/app/src/androidTest/java/com/example/trail/NewTask/AddTaskActivityTest.java
@@ -0,0 +1,24 @@
+package com.example.trail.NewTask;
+
+import androidx.test.rule.ActivityTestRule;
+
+import com.example.trail.Lists.SideMenuActivity;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddTaskActivityTest {
+    @Rule
+    public ActivityTestRule<AddTaskActivity> rule = new ActivityTestRule<>(AddTaskActivity.class);
+
+    @Before
+    public void setUp() throws Exception {
+    }
+    @Test
+    public void testAttr(){
+
+    }
+}
