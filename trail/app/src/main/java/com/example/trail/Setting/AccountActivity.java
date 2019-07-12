4243baacb1834080b6dd9cc59479c0cd1fd1945e
Saturn0324 <Forever0324@sjtu.edu.cn>
2019-07-12 14:12:18 +0800

    account set

--- trail/app/src/main/java/com/example/trail/Setting/AccountActivity.java
+++ trail/app/src/main/java/com/example/trail/Setting/AccountActivity.java
@@ -2,7 +2,9 @@ package com.example.trail.Setting;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,34 +12,68 @@ import android.widget.Toast;
 
 import com.example.trail.R;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 public class AccountActivity extends AppCompatActivity {
     private Button btnLogin;
     private Button btnRegister;
     private EditText username;
     private EditText password;
+    final  private String FILE_NAME = "information.txt";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
         btnLogin=findViewById(R.id.btn_login);
         btnRegister=findViewById(R.id.btn_register);
-        btnLogin.setOnClickListener(new View.OnClickListener() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {//登录
             @Override
             public void onClick(View view) {
-                Toast.makeText(AccountActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
+                if(getUsername().length()<=0){ //是否输入密码
+                    Toast.makeText(AccountActivity.this,"请输入用户名",Toast.LENGTH_SHORT).show();
+                }
+                else if(getPassword().length()<=0){
+                    Toast.makeText(AccountActivity.this,"请输入密码",Toast.LENGTH_SHORT).show();
+                }
+                else {//判断是否符合条件
+                    if(!searchAccount(getUsername())){
+                        Toast.makeText(AccountActivity.this,"不存在此用户，请注册",Toast.LENGTH_SHORT).show();
+                    }
+                    else if(!searchPassword(getUsername(),getPassword())){
+                        Toast.makeText(AccountActivity.this,"密码错误，请重新输入",Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        Toast.makeText(AccountActivity.this,"登录成功",Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
-        btnRegister.setOnClickListener(new View.OnClickListener() {
+        btnRegister.setOnClickListener(new View.OnClickListener() {//注册
             @Override
             public void onClick(View view) {
-                Toast.makeText(AccountActivity.this,"注册成功",Toast.LENGTH_SHORT).show();
+                if(getUsername().length()<=0){//用户名 密码不能为空
+                    Toast.makeText(AccountActivity.this,"用户名不能为空",Toast.LENGTH_SHORT).show();
+                }
+                else if(getPassword().length()<=0){//用户名 密码不能为空
+                    Toast.makeText(AccountActivity.this,"密码不能为空",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(AccountActivity.this,"注册成功",Toast.LENGTH_SHORT).show();
+                    String inputAccount="Account: "+getUsername();
+                    save(inputAccount);
+                    String inputPassword="Password: "+getPassword();
+                    save(inputPassword);
+                }
             }
         });
     }
 
     public String getUsername(){
         username=findViewById(R.id.et_username);
-        String un=username.getText().toString();
+        String un="";
+        un=username.getText().toString();
         return un;
     }
 
@@ -45,6 +81,59 @@ public class AccountActivity extends AppCompatActivity {
         password=findViewById(R.id.et_password);
         String pw=password.getText().toString();
         return pw;
+    }
+
+    public void save(String text) {
+        try {
+            FileOutputStream fos = openFileOutput(FILE_NAME, Context.MODE_APPEND);
+            fos.write(text.getBytes());
+            fos.write("\n".getBytes());//格式化
+            fos.flush();
+            fos.close();
+
+        } catch (Exception e) {
+            Log.d("errMsg", e.toString());
+        }
+    }
+
+    private String readFile(){
+        String textContent="";
+        try {
+            FileInputStream ios = openFileInput(FILE_NAME);
+            byte[] temp = new byte[1024];
+            StringBuilder sb = new StringBuilder("");
+            int len = 0;
+            while ((len = ios.read(temp)) > 0){
+                sb.append(new String(temp, 0, len));
+            }
+            ios.close();
+            textContent= sb.toString();
+        }catch (Exception e) {
+            Log.d("errMsg", e.toString());
+        }
+        return textContent;
+    }
+    //查询text
+    private boolean searchFile(String text){
+        String textContent=readFile();
+        if(textContent.contains(text))return true;
+        else return false;
+    }
+
+    //用户名判断
+    private boolean searchAccount(String account){
+        String text=readFile();
+        String pattern="Account: "+account+"\n";
+        if(text.contains(pattern)) return true;
+        else return false;
+    }
+
+    //密码判断
+    private boolean searchPassword(String account,String password){
+        String textContent=readFile();
+        String pattern="Account: "+account+"\nPassword: "+password+"\n";
+        if(textContent.contains(pattern))return true;
+        else return false;
     }
 
 }
--- trail/app/src/main/java/com/example/trail/Setting/VoiceActivity.java
+++ trail/app/src/main/java/com/example/trail/Setting/VoiceActivity.java
@@ -1,14 +1,22 @@
 package com.example.trail.Setting;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
+import android.app.Service;
 import android.content.Context;
 import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Switch;
 
 import com.example.trail.R;
+
+import static java.nio.file.attribute.AclEntryType.ALARM;
 
 public class VoiceActivity extends AppCompatActivity {
     private SeekBar sbVoice0;
@@ -21,21 +29,103 @@ public class VoiceActivity extends AppCompatActivity {
         setContentView(R.layout.activity_voice);
 
         sbVoice0=findViewById(R.id.sb_voice0);
-        sbVoice0.setMax(100);
-        sbVoice0.setProgress(50);
+        sbVoice0.setMax(getMaxSystemVolume());
+        sbVoice0.setProgress(getSystemVolume());
+        sbVoice0.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int systemVoice=sbVoice0.getProgress();
+                changeSystemVolume(systemVoice);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
 
         sbVoice1=findViewById(R.id.sb_voice1);
-        sbVoice1.setMax(100);
-        sbVoice1.setProgress(50);
+        sbVoice1.setMax(getMaxMusicVolume());
+        int max=getMaxMusicVolume();
+        Log.d("ALARM","Max:"+max);
+        sbVoice1.setProgress(getMusicVolume());
+        sbVoice1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int musicVoice=sbVoice1.getProgress();
+                changeMusicVolume(musicVoice);
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) { }
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
 
         vibra=findViewById(R.id.vibration);
+        vibra.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            Vibrator vb = (Vibrator)getSystemService(Service.VIBRATOR_SERVICE);
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if(b){
+                    vb.cancel();
+                }
+            }
+        });
     }
 
-    private void changeStreamVolume(int i){
+
+
+    //改变系统音量
+    private void changeSystemVolume(int voice){
         int max=0,current=0;
         mAudioManager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
         max = mAudioManager.getStreamMaxVolume( AudioManager.STREAM_SYSTEM );
         current = mAudioManager.getStreamVolume( AudioManager.STREAM_SYSTEM );
-        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, current, AudioManager.FLAG_PLAY_SOUND);
+        Log.d("ALARM","MAX: "+max+" current: "+current);
+        mAudioManager.setStreamVolume(AudioManager.STREAM_SYSTEM, voice, AudioManager.FLAG_PLAY_SOUND);
+        current = mAudioManager.getStreamVolume( AudioManager.STREAM_SYSTEM );
+        Log.d("ALARM","MAX: "+max+" current: "+current);
+    }
+
+    //获取当前系统音量
+    private int getSystemVolume(){
+        int volume=0;
+        mAudioManager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        volume = mAudioManager.getStreamVolume( AudioManager.STREAM_SYSTEM );
+        return volume;
+    }
+
+    //获取系统最大音量
+    private int getMaxSystemVolume(){
+        int maxVolume=0;
+        mAudioManager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        maxVolume = mAudioManager.getStreamMaxVolume( AudioManager.STREAM_SYSTEM );
+        return maxVolume;
+    }
+
+    //获取当前媒体音量
+    private int getMusicVolume(){
+        int volume=0;
+        mAudioManager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        volume = mAudioManager.getStreamVolume( AudioManager.STREAM_MUSIC );
+        return volume;
+    }
+
+    //获取最大媒体音量
+    private int getMaxMusicVolume(){
+        int maxVolume=0;
+        mAudioManager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        maxVolume=mAudioManager.getStreamMaxVolume( AudioManager.STREAM_MUSIC );
+        return maxVolume;
+    }
+    //改变媒体音量
+    private void changeMusicVolume(int voice){
+        int max=0,current=0;
+        mAudioManager=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        max = mAudioManager.getStreamMaxVolume( AudioManager.STREAM_MUSIC );
+        current = mAudioManager.getStreamVolume( AudioManager.STREAM_MUSIC );
+        Log.d("ALARM","MAX: "+max+" current: "+current);
+        mAudioManager.setStreamVolume(AudioManager.STREAM_MUSIC, voice, AudioManager.FLAG_PLAY_SOUND);
+        current = mAudioManager.getStreamVolume( AudioManager.STREAM_MUSIC );
+        Log.d("ALARM","MAX: "+max+" current: "+current);
     }
 }
