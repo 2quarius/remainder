327dba3a46af29f7775572c0f93a37255821fc58
Mosen Cao <979077305@qq.com>
2019-07-17 15:28:25 +0800

    delete NewTaskActivity

--- trail/app/src/main/java/com/example/trail/TimeRemind/RemindActivity.java
+++ trail/app/src/main/java/com/example/trail/TimeRemind/RemindActivity.java
@@ -2,13 +2,7 @@ package com.example.trail.TimeRemind;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 import com.example.trail.R;
 
--- trail/app/src/main/java/com/example/trail/TimeRemind/TimeRemindService.java
+++ trail/app/src/main/java/com/example/trail/TimeRemind/TimeRemindService.java
@@ -5,10 +5,12 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.os.SystemClock;
 
 import com.example.trail.NewTask.SimpleTask.RemindCycle;
 import com.example.trail.NewTask.SimpleTask.Task;
 
+import java.util.Calendar;
 import java.util.Date;
 
 public class TimeRemindService extends Service {
@@ -21,23 +23,28 @@ public class TimeRemindService extends Service {
     private void addTasktoAlarm() {
         Date timesetted = task.remindTime;
         Date timenow = new Date(System.currentTimeMillis());
+        Calendar cal = Calendar.getInstance();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(timesetted);
         long timerange = timesetted.getTime() - timenow.getTime();
-        if (task.remindCycle == RemindCycle.SINGLE) {
-            if (timerange>=0) {
-                alarm.set(AlarmManager.RTC_WAKEUP, timesetted.getTime(), remind);
+        if (timerange>=0) {
+            if (task.remindCycle == RemindCycle.SINGLE) {
+                long diff=cal.getTimeInMillis()- SystemClock.elapsedRealtime();
+                alarm.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis()-diff, remind);
             }
-        }
-        else {
-            long cyclingTime = 1000000000;
-            switch (task.remindCycle) {
-                case DAILY:
-                    cyclingTime = 1000 * 60 * 60 * 24;
-                    break;
-                case WEEKLY:
-                    cyclingTime = 1000 * 60 * 60 * 24 * 7;
-                    break;
+            else {
+                long cyclingTime = 1000000000;
+                switch (task.remindCycle) {
+                    case DAILY:
+                        cyclingTime = 1000 * 60 * 60 * 24;
+                        break;
+                    case WEEKLY:
+                        cyclingTime = 1000 * 60 * 60 * 24 * 7;
+                        break;
+                }
+                long diff=cal.getTimeInMillis()- SystemClock.elapsedRealtime();
+                alarm.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis()-diff, cyclingTime, remind);
             }
-            alarm.setRepeating(AlarmManager.RTC_WAKEUP, timesetted.getTime(), cyclingTime, remind);
         }
     }
 
