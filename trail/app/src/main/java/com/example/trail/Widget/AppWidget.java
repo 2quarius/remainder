518804319657275d7cc593bb1b3fd6d70de32ec3
Saturn0324 <Forever0324@sjtu.edu.cn>
2019-07-18 08:48:41 +0800

    widget pre

---
+++ trail/app/src/main/java/com/example/trail/Widget/AppWidget.java
@@ -0,0 +1,44 @@
+package com.example.trail.Widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.widget.RemoteViews;
+
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.trail.R;
+
+
+public class AppWidget extends AppWidgetProvider {
+    private ViewPager view;
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.app_widget);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
---
+++ trail/app/src/main/java/com/example/trail/Widget/WeidgetListFragment.java
@@ -0,0 +1,6 @@
+package com.example.trail.Widget;
+
+import androidx.fragment.app.Fragment;
+
+public class WeidgetListFragment extends Fragment {
+}
