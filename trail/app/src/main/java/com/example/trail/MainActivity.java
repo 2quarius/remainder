49ab4059019b46c650349bc4080e05396abfa615
Mosen Cao <979077305@qq.com>
2019-07-19 09:07:15 +0800

    nothing

--- trail/app/src/main/java/com/example/trail/MainActivity.java
+++ trail/app/src/main/java/com/example/trail/MainActivity.java
@@ -24,8 +24,6 @@ import com.example.trail.Services.AlarmManageService;
 import com.example.trail.Services.BaiduMapService;
 import com.example.trail.Setting.SettingFragmnet;
 import com.example.trail.TimeRemind.TimeRemindService;
-import com.example.trail.Utility.StoreRetrieveData;
-import com.example.trail.Utility.TabConstants;
 import com.example.trail.Utility.DataStorageHelper.StoreRetrieveData;
 import com.example.trail.Utility.EnumPack.TabConstants;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
